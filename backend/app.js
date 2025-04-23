b7643bd2852ca281b52f645de986da9cbb6efc27
VibhorSharma0053 <144813390+VibhorSharma0053@users.noreply.github.com>
2025-04-23 19:17:44 +0530

    Backend init and login and signup endpoint

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoute = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+// Routes
+app.use('/api/auth', authRoute);
+
+// MongoDB Connection using Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected successfully.");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+});
